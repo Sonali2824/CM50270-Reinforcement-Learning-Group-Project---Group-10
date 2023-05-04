45d88e5a0fc7cd479c955970fa582b0c7c81050c
Sonali Nandagopalan <57223104+Sonali2824@users.noreply.github.com>
2023-05-04 10:56:47 +0100

    A2C Flappy Bird Python Files
    A2C Flappy Bird training, evaluating and network code.

---
+++ a2c_main.py
@@ -0,0 +1,158 @@
+import os
+import torch
+import numpy as np 
+from tensorboardX import SummaryWriter
+from collections import namedtuple
+
+from game.wrapper import Game
+from network import Actor, Critic
+from a2c_network import ActorCriticNetwork
+import torch.nn.functional as F
+
+def optimise(buffer):
+
+    action_tensor = torch.stack(buffer.action)
+    #action_log_prob_tensor = torch.stack(buffer.action_log_prob).detach()
+    reward_tensor = torch.tensor(buffer.reward)
+    state_tensor = torch.stack(buffer.state)
+    termination_tensor = torch.stack(buffer.termination)
+    #val_tensor = torch.stack(buffer.val ).detach()
+
+    batch_data = {
+        'action': action_tensor,
+        'reward': reward_tensor,
+        'state': state_tensor,
+        'termination': termination_tensor
+    }
+
+    state_dim = batch_data['state'].size()[2:]
+    action_dim = batch_data['action'].size()[-1]
+
+    next_value, _ = net(batch_data['state'][-1])
+
+    returns = torch.zeros(hyperparameters["update_freq"] + 1, hyperparameters["workers"], 1)
+    returns[-1] = next_value
+    for i in reversed(range(hyperparameters["update_freq"])):
+        returns[i] = returns[i+1] * hyperparameters["gamma"] * batch_data['termination'][i] + batch_data['reward'][i]
+    returns = returns[:-1]
+
+    vals, action_log_probabilities, distrib_entropy = net.action_evaluation(batch_data['state'].view(-1, *state_dim), batch_data['action'].view(-1, action_dim)) 
+    vals = vals.view(hyperparameters["update_freq"], hyperparameters["workers"], 1)
+    action_log_probabilities = action_log_probabilities.view(hyperparameters["update_freq"], hyperparameters["workers"], 1)
+
+    advs = returns - vals
+    val_loss = advs.pow(2).mean()
+
+    action_loss = -(advs*action_log_probabilities).mean()
+
+
+    total_loss = hyperparameters["value_loss_coefficient"] * val_loss + action_loss - distrib_entropy * hyperparameters["entropy_coefficient"]
+
+    optimizer.zero_grad()
+    total_loss.backward()
+    torch.nn.utils.clip_grad_norm(net.parameters(), hyperparameters["norm_clip_grad"])
+    optimizer.step()
+
+    val_loss = val_loss * hyperparameters["value_loss_coefficient"]
+    distrib_entropy = - distrib_entropy * hyperparameters["entropy_coefficient"]
+
+    return total_loss, val_loss, action_loss, distrib_entropy 
+
+
+# Environment specific env_step function
+def env_step( current_states, actions):
+    next_state_list, reward_list, done_list = [], [], []
+    for i in range(hyperparameters["workers"]):
+        frame, reward, done = game_simulations[i].step(actions[i])
+        if current_states is None:
+            next_state = torch.cat([frame for i in range(hyperparameters["no_frames_to_network"])])
+        else:
+            next_state = torch.cat([current_states[i][1:], frame])
+        next_state_list.append(next_state)
+        reward_list.append([reward])
+        done_list.append(done)
+
+    return torch.stack(next_state_list), reward_list, done_list
+
+if __name__ == '__main__': 
+
+    hyperparameters = {
+    "logs_dir":"exp_test_5",
+    "model_dir":"",
+    "no_train_iterations":100000,
+    "lr":3e-4,
+    "no_frames_to_network":4,
+    "gamma":0.99,
+    "workers":8,
+    "update_freq":20,
+    "entropy_coefficient":0.01,
+    "value_loss_coefficient":0.5,
+    "norm_clip_grad":40,
+    "clip":0.1,
+    "log_freq":100,
+    "save_freq":1000,
+    "actions_n":2,
+    "frame_size":84
+    }
+
+    Experience = namedtuple('Experience', ('state', 'action', 'action_log_prob', 'val', 'reward', 'termination'))
+
+    CUDA_DEVICE = torch.cuda.is_available()
+
+   
+    net = ActorCriticNetwork(hyperparameters)
+    net.apply(net.init_weights)
+    
+
+    if CUDA_DEVICE:
+        net = net.cuda()
+
+
+    optimizer = torch.optim.Adam(net.parameters(), lr=hyperparameters["lr"])
+
+    game_simulations = [Game(hyperparameters["frame_size"]) for i in range(hyperparameters["workers"])]
+
+    scalar_writer = SummaryWriter(hyperparameters["logs_dir"])
+
+    buffer = []
+    
+    #Training starts here
+    ep_lens = np.zeros(hyperparameters["workers"])
+    starting_actions = np.zeros(hyperparameters["workers"])
+    current_states,rewards_, done_ = env_step(None, starting_actions)
+
+    for current_itr in range(1, hyperparameters["no_train_iterations"]):
+
+        vals, actions, action_log_probabilities = net.take_action(current_states)
+
+        next_states, rewards, completions = env_step(current_states, actions)
+        terminations = torch.FloatTensor([[0.0] if completed else [1.0] for completed in completions])
+
+        buffer.append(Experience(current_states.data, actions.data, action_log_probabilities.data, vals.data, rewards, terminations))
+
+        if current_itr % hyperparameters["update_freq"] == 0:
+            total_loss, val_loss, action_loss, entropy_loss = optimise(Experience(*zip(*buffer)))
+            buffer = []
+
+        for worker in range(hyperparameters["workers"]):
+            if not completions[worker]:
+                ep_lens[worker] += 1
+            else:
+                scalar_writer.add_scalar('episode_length/' + str(worker), ep_lens[worker], current_itr)
+                print(worker, ep_lens[worker])
+                ep_lens[worker] = 0
+
+        if current_itr % hyperparameters["save_freq"] == 0:
+            if not os.path.exists(hyperparameters["logs_dir"]):
+                os.mkdir(hyperparameters["logs_dir"])
+            torch.save(net.state_dict(), f'{hyperparameters["logs_dir"]}/{str(current_itr).zfill(7)}.pt')
+
+        if current_itr % hyperparameters["log_freq"] == 0:
+            scalar_writer.add_scalar('loss/total', total_loss, current_itr)
+            scalar_writer.add_scalar('loss/action', action_loss, current_itr)
+            scalar_writer.add_scalar('loss/value', val_loss, current_itr)
+            scalar_writer.add_scalar('loss/entropy', entropy_loss, current_itr)
+            
+
+        current_states = next_states
+   
---
+++ a2c_network.py
@@ -0,0 +1,136 @@
+import torch
+# Heatmap Imports
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import cv2
+import numpy as np
+
+class ActorCriticNetwork(torch.nn.Module):
+
+    def __init__(self, hyperparameters):
+        """
+        Initialize an ActorCriticNetworkself.opt instance. The actor has an output for 
+        each action and the critic provides the value output
+        Uses the same parameters as specified in the paper.
+        """
+        super(ActorCriticNetwork, self).__init__()
+
+        # self.opt = options
+        
+        self.convolution_1 = torch.nn.Conv2d(hyperparameters["no_frames_to_network"], 16, 8, 4)
+        self.relu_1 = torch.nn.ReLU()
+        self.convolution_2 = torch.nn.Conv2d(16, 32, 4, 2)
+        self.relu_2 = torch.nn.ReLU()
+        self.fully_connected_layer_3 = torch.nn.Linear(2592, 256) # TODO: Don't hard code
+        self.relu_3 = torch.nn.ReLU()
+        
+        self.convolution_4 = torch.nn.Conv2d(hyperparameters["no_frames_to_network"], 16, 8, 4)
+        self.relu_4 = torch.nn.ReLU()
+        self.convolution_5 = torch.nn.Conv2d(16, 32, 4, 2)
+        self.relu_5 = torch.nn.ReLU()
+        self.fully_connected_layer_6 = torch.nn.Linear(2592, 256) # TODO: Don't hard code
+        self.relu_6 = torch.nn.ReLU()
+        
+        self.actor = torch.nn.Linear(256, hyperparameters["actions_n"])
+        self.critic = torch.nn.Linear(256, 1)
+        self.softmax = torch.nn.Softmax()
+        self.logsoftmax = torch.nn.LogSoftmax()
+
+
+    def init_weights(self, m):
+        """
+        Initialize the weights of the network.
+
+        Arguments:
+            m (tensor): layer instance 
+        """
+        if type(m) == torch.nn.Conv2d or type(m) == torch.nn.Linear:
+            torch.nn.init.uniform(m.weight, -0.01, 0.01)
+            m.bias.data.fill_(0.01)
+
+
+    def forward(self, x):
+        """
+        Forward pass to compute Q-values for given input states.
+
+        Arguments:
+            x (tensor): minibatch of input states
+
+        Returns:
+            int: selected action, 0 to do nothing and 1 to flap
+            float: entropy of action space
+            float: log probability of selecting the action 
+            float: value of the particular state
+        """
+        # Forward pass
+        x_input = x
+        x_a = self.relu_1(self.convolution_1(x_input))
+        x_a = self.relu_2(self.convolution_2(x_a))
+        x_1 = x_a
+        x_a = x_a.view(x_a.size()[0], -1)
+        x_a = self.relu_3(self.fully_connected_layer_3(x_a))
+        
+        x_c = self.relu_4(self.convolution_4(x_input))
+        x_c = self.relu_5(self.convolution_5(x_c))
+        x_c = x_c.view(x_c.size()[0], -1)
+        x_c = self.relu_6(self.fully_connected_layer_6(x_c))
+        
+        action_logits = self.actor(x_a)
+        value = self.critic(x_c)
+
+        # input
+        image_rescaled = (255*x_input[0][0]).clamp(0, 255).byte()  # Rescale and convert to byte tensor
+        image_rgb = image_rescaled.unsqueeze(2).repeat(1, 1, 3)  # Add a third dimension and repeat values along it
+        cv2.imwrite('input_image1.png', image_rgb.numpy())  # Save the image using cv2.imwrite()
+        
+        # Compute heatmap
+        x_1_np = x_1.detach().cpu().numpy()
+        heatmap = np.mean(x_1_np, axis=1)  # Average over channels
+        heatmap = np.squeeze(heatmap)  # Remove the channel dimension
+        heatmap = (heatmap - np.min(heatmap)) / (np.max(heatmap) - np.min(heatmap))  # Normalize to [0, 1]
+
+        # Convert the heatmap to RGBA
+        cmap = cm.get_cmap('jet')
+        rgba_heatmap = cmap(heatmap)
+
+        # Convert RGBA to BGR
+        bgr_heatmap = cv2.cvtColor((rgba_heatmap * 255).astype(np.uint8), cv2.COLOR_RGBA2BGR)
+        
+        # Upscale the heatmap
+        upscale_factor = 10
+        new_height, new_width = bgr_heatmap.shape[0] * upscale_factor, bgr_heatmap.shape[1] * upscale_factor
+        bgr_heatmap_resized = cv2.resize(bgr_heatmap, (new_width, new_height), interpolation=cv2.INTER_LINEAR)
+
+        # Save the upscaled heatmap
+        cv2.imwrite('heatmap1.png', bgr_heatmap_resized)
+        return value, action_logits
+
+
+    def take_action(self, x):
+        """
+        Returns:
+            tensor(8,1)
+        """
+        # Forward pass
+        vals, action_logits = self.forward(x)
+        probs = self.softmax(action_logits)
+        log_probs = self.logsoftmax(action_logits)
+
+        # Choose action stochastically
+        actions = probs.multinomial(1)
+
+        # Evaluate action
+        action_log_probs = log_probs.gather(1, actions)
+        dist_entropy = -(log_probs * probs).sum(-1).mean()
+        return vals, actions, action_log_probs
+
+    def action_evaluation(self, x, actions):
+        # Forward pass 
+        value, action_logits = self.forward(x)
+        probs = self.softmax(action_logits)
+        log_probs = self.logsoftmax(action_logits)
+
+        # Evaluate actions
+        action_log_probs = log_probs.gather(1, actions)
+        dist_entropy = -(log_probs * probs).sum(-1).mean()
+        return value, action_log_probs, dist_entropy
